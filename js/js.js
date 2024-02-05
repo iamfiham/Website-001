2cf353ad713193e0309168a257350b84700928d6
iamfiham <xyzfiham@gmail.com>
2024-02-05 21:09:40 +0530

    correct alignment and spaces

--- js/js.js
+++ js/js.js
@@ -1,28 +1,27 @@
-const root = document.querySelector(':root');
+const root = document.querySelector(":root");
 
-window.addEventListener("resize", () => { 
-    console.log(element.getBoundingClientRect().top);
-    console.log(element.getBoundingClientRect().bottom);
-    console.log(element.getBoundingClientRect().height);
- });
+window.addEventListener("resize", () => {
+  console.log(element.getBoundingClientRect().top);
+  console.log(element.getBoundingClientRect().bottom);
+  console.log(element.getBoundingClientRect().height);
+});
 
-window.addEventListener("mousemove", (para) => { 
-    let x = para.clientX;
-    let y = para.clientY;
-    // console.log(x);
-    // console.log(y);
-    let vh = window.innerHeight;
-    let vw = window.innerWidth;
-    // console.log(vh);
-    // console.log(vw);
-    
-    let ox = ((x - vw/2)/(vw/2)*30);
-    let oy = ((y - vh/2)/(vh/2)*-30);
+window.addEventListener("mousemove", (para) => {
+  let x = para.clientX;
+  let y = para.clientY;
+  // console.log(x);
+  // console.log(y);
+  let vh = window.innerHeight;
+  let vw = window.innerWidth;
+  // console.log(vh);
+  // console.log(vw);
 
-    root.style.setProperty("--x", ox + "deg")
-    root.style.setProperty("--y", oy + "deg")
+  let ox = Math.round(((x - vw / 2) / (vw / 2)) * 30);
+  let oy = Math.round(((y - vh / 2) / (vh / 2)) * -30);
 
-    // console.log(ox);
-    // console.log(oy);
-    
- });
+  root.style.setProperty("--x", ox + "deg");
+  root.style.setProperty("--y", oy + "deg");
+
+  // console.log(ox);
+  // console.log(oy);
+});
