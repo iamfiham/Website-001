efb97c655189dfe82e3dea0a7d73e2238eadec5a
iamfiham <xyzfiham@gmail.com>
2024-02-12 16:18:58 +0530

    minor changes in circle animation

--- js/cursor animation.js
+++ js/cursor animation.js
@@ -2,14 +2,22 @@ let circle = document.getElementById("cursor");
 let heading = document.getElementById("gsap-box");
 let body = document.body;
 
-let tophead = heading.getBoundingClientRect().top;
-let lefthead = heading.getBoundingClientRect().left;
-let righthead = lefthead + heading.getBoundingClientRect().width;
-let bottomhead = tophead + heading.getBoundingClientRect().height;
-
 window.addEventListener("mousemove", (e) => {
   let cursorx = e.pageX;
   let cursory = e.pageY;
+
+  let tophead = window.scrollY + heading.getBoundingClientRect().top;
+  let lefthead = heading.getBoundingClientRect().left;
+  let righthead = lefthead + heading.getBoundingClientRect().width;
+  let bottomhead = tophead + heading.getBoundingClientRect().height;
+
+  // console.log("x" + cursorx);
+  // console.log("y" + cursory);
+  // console.log("top" + tophead);
+  // console.log("left" + lefthead);
+  // console.log("right" + righthead);
+  // console.log("bottom" + bottomhead);
+
   let circlewidth = getComputedStyle(circle).width.slice(0, -2);
 
   circle.style.left = cursorx - circlewidth / 2 + "px";
