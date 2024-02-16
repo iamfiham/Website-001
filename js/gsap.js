48b5a7cdd40a6800640a682e184780761ca14e1b
iamfiham <xyzfiham@gmail.com>
2024-02-16 20:28:32 +0530

    rocket animation start

--- js/gsap.js
+++ js/gsap.js
@@ -1,6 +1,7 @@
 const Box = document.getElementById("gsap-box");
+
+// SECTION heading
 let size = Box.getBoundingClientRect().top + "px";
-console.log(size);
 gsap.registerPlugin(ScrollTrigger);
 
 let tl = gsap.timeline({
@@ -13,7 +14,7 @@ let tl = gsap.timeline({
     // markers: true,
     markers: false,
     // pin: true,
-    toggleActions: "play reverse play reverse",
+    // toggleActions: "play reverse play reverse",
   },
 });
 tl.to(Box, {
@@ -21,4 +22,35 @@ tl.to(Box, {
   // scale: 1.1,
   autoAlpha: 0,
   duration: 0.5,
+  ease: "none",
+});
+
+// SECTION ROCKET
+const rocket = document.getElementById("rocket");
+const container = document.getElementById("container");
+
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: rocket,
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 2,
+    // markers: true,
+    markers: false,
+  },
+});
+tl2.to(rocket, {
+  x: () => {
+    return (
+      container.getBoundingClientRect().width -
+      rocket.getBoundingClientRect().width
+    );
+  },
+});
+
+window.addEventListener("resize", () => {
+  let progress = tl2.totalProgress();
+  tl2.progress(0);
+  tl2.invalidate();
+  tl2.totalProgress(progress);
 });
--- js/home section height.js
+++ js/home section height.js
@@ -4,11 +4,9 @@ let nav = document.getElementById("nav");
 window.addEventListener("load", (event) => {
   home.style.minHeight =
     window.innerHeight - nav.getBoundingClientRect().height + "px";
-  console.log(home.style.minHeight);
 });
 
 window.addEventListener("resize", () => {
   home.style.minHeight =
     window.innerHeight - nav.getBoundingClientRect().height + "px";
-  console.log(home.style.minHeight);
 });
